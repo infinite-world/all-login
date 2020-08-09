93c8dfe786545e6a01eb1fd5795994bc279d9c1b
Abdul Rehman <65752346+infinite-world@users.noreply.github.com>
2020-08-09 04:18:41 -0700

    Add files via upload

---
+++ login/Login.js
@@ -0,0 +1,139 @@
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import {
+  MDBNavbar,
+  MDBNavbarBrand,
+  MDBNavbarNav,
+  MDBNavItem,
+  MDBNavLink,
+  MDBNavbarToggler,
+  MDBCollapse,
+  MDBMask,
+  MDBRow,
+  MDBCol,
+  MDBIcon,
+  MDBBtn,
+  MDBView,
+  MDBContainer,
+  MDBCard,
+  MDBCardBody,
+  MDBInput,
+  MDBFormInline,
+  MDBAnimation,
+} from "mdbreact";
+import "./Login.scss";
+
+class ClassicFormPage extends React.Component {
+  state = {
+    collapseID: "",
+  };
+
+  toggleCollapse = (collapseID) => () =>
+    this.setState((prevState) => ({
+      collapseID: prevState.collapseID !== collapseID ? collapseID : "",
+    }));
+
+  render() {
+    const overlay = (
+      <div
+        id="sidenav-overlay"
+        style={{ backgroundColor: "transparent" }}
+        onClick={this.toggleCollapse("navbarCollapse")}
+      />
+    );
+    return (
+      <div id="classicformpage">
+        <Router>
+          <div>
+            <MDBNavbar dark expand="md" fixed="top">
+              <MDBContainer>
+                <MDBNavbarBrand>
+                  <strong className="white-text">WebTailors</strong>
+                </MDBNavbarBrand>
+              </MDBContainer>
+            </MDBNavbar>
+            {this.state.collapseID && overlay}
+          </div>
+        </Router>
+
+        <MDBView>
+          <MDBMask className="d-flex justify-content-center align-items-center gradient">
+            <MDBContainer>
+              <MDBRow>
+                <MDBAnimation
+                  type="fadeInLeft"
+                  delay=".3s"
+                  className="white-text text-center text-md-left col-md-6 mt-xl-5 mb-5"
+                >
+                  <h1 className="h1-responsive font-weight-bold">
+                    User Log In!
+                  </h1>
+                  <hr className="hr-light" />
+                  <h6 className="mb-4">
+                    A Branding Company For Connectiong Tailors Togather
+                  </h6>
+                </MDBAnimation>
+
+                <MDBCol md="6" xl="5" className="mb-4">
+                  <MDBAnimation type="fadeInRight" delay=".3s">
+                    <MDBCard id="classic-card">
+                      <MDBCardBody className="white-text">
+                        <h3 className="text-center">
+                          <MDBIcon icon="user" /> User log In:
+                        </h3>
+                        <hr className="hr-light" />
+                        <MDBInput
+                          className="white-text"
+                          iconClass="white-text"
+                          label="Your name"
+                          icon="user"
+                        />
+
+                        <MDBInput
+                          className="white-text"
+                          iconClass="white-text"
+                          label="Your password"
+                          icon="lock"
+                          type="password"
+                        />
+                        <div className="text-center mt-4 black-text">
+                          <MDBBtn color="indigo">Log In</MDBBtn>
+                          <hr className="hr-light" />
+                          <div className="text-center d-flex justify-content-center white-label">
+                            <a href="#!" className="p-2 m-2">
+                              <MDBIcon
+                                fab
+                                icon="twitter"
+                                className="white-text"
+                              />
+                            </a>
+                            <a href="#!" className="p-2 m-2">
+                              <MDBIcon
+                                fab
+                                icon="linkedin"
+                                className="white-text"
+                              />
+                            </a>
+                            <a href="#!" className="p-2 m-2">
+                              <MDBIcon
+                                fab
+                                icon="instagram"
+                                className="white-text"
+                              />
+                            </a>
+                          </div>
+                        </div>
+                      </MDBCardBody>
+                    </MDBCard>
+                  </MDBAnimation>
+                </MDBCol>
+              </MDBRow>
+            </MDBContainer>
+          </MDBMask>
+        </MDBView>
+      </div>
+    );
+  }
+}
+
+export default ClassicFormPage;
